c93f162816d0c238326f640933fd1ca5ca9c04d2
natsuume <natsuume@natsuume.dev>
2020-07-14 23:25:08 +0900

    サーバレスに切り替えるのでサーバサイド削除

--- server/src/main/java/dev/natsuume/web/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.natsuume.web.repository;
-
-import dev.natsuume.web.data.BookItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<BookItem, Integer> {
-}
